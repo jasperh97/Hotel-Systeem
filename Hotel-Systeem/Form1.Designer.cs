d28e863cdab80a0b13a95737c8dba5dceb815c86
Jasper H <j.hetterscheid@gmail.com>
2018-12-07 14:01:58 +0100

    Merge conflict example

--- Hotel-Systeem/Form1.Designer.cs
+++ Hotel-Systeem/Form1.Designer.cs
@@ -29,36 +29,23 @@
         private void InitializeComponent()
         {
             this.lblTest = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblTest
             // 
             this.lblTest.AutoSize = true;
-            this.lblTest.Location = new System.Drawing.Point(409, 160);
-            this.lblTest.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTest.Location = new System.Drawing.Point(307, 130);
             this.lblTest.Name = "lblTest";
-            this.lblTest.Size = new System.Drawing.Size(71, 17);
+            this.lblTest.Size = new System.Drawing.Size(56, 13);
             this.lblTest.TabIndex = 0;
             this.lblTest.Text = "GIT TEST";
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(474, 235);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.lblTest);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -69,7 +56,6 @@
         #endregion
 
         private System.Windows.Forms.Label lblTest;
-        private System.Windows.Forms.Button button1;
     }
 }
 
